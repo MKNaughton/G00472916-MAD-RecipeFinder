e80fcd1908310833cb88f2cf407727736ee68812
MK Naughton <naughtonmarykerin@gmail.com>
2026-01-05 20:42:08 +0000

    Set up API Service to connect to Spoonacular

---
+++ src/app/services/recipe-api.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+//https://capacitorjs.com/docs/apis/http#httpoptions
+import { CapacitorHttp, HttpResponse, HttpOptions} from '@capacitor/core';
+
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RecipeApiService {
+
+  constructor(){}
+// tells javascript this function waits for internet data dont stall app-class materials
+  async get(options:HttpOptions){
+    return await //pauses untill capaitorHttp finishes getting data
+    CapacitorHttp.get(options);//capacitor funcition that gets the data from the url 
+  }
+  
+}
