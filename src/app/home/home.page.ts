9237343eb48bd38d1cd56e58c406fa91323c532e
MK Naughton <naughtonmarykerin@gmail.com>
2026-01-06 15:19:12 +0000

    Created Recipe Interface+recipe array-type structure for API response data-Parse response.data.results into typed array for display

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,6 +11,19 @@ import {FormsModule} from '@angular/forms';
 
 // Every <ion-something> used in the HTML must be imported in the .ts file.
 //Ref: https://ionicframework.com/docs/components
+
+// Creating a typeScript inteface to define the structure of recipe data and an array to store multiple recipes returned from the API.
+//typeScript interfaces provide both type safty and intellisense autocomplete for ingredients input bar.
+//The array enables *ngFor in Html to loop through and display multiple arrays.
+//REF:W3Schools TypeScript Interfaces - https://www.w3schools.com/typescript/typescript_object_types.php
+//REF: Class materials wk 11 JSON demos
+interface Recipe {
+  id: number; //recipe ID 
+  title: string; //recipe name to display
+  image: string;//Image URL to display the recipe photo
+
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -25,6 +38,10 @@ export class HomePage {
 
   //Spoonacular Api Key 
   apiKey: string = '70759a4f7911402abcc53d3c51d3b759';
+
+//Array to store recipe results from API- REF : class materials wk 11 demo data in array
+recipes: Recipe[] = [];
+
 //constructor uses dependancy injection to get ReciepeApiService instance 
   constructor(private recipeApi: RecipeApiService) {}
 
@@ -33,16 +50,24 @@ export class HomePage {
   async searchRecipes(){
   
     const options: HttpOptions = {
-
+     //HttpOption object which has the API url
       //constructor URL: base + apiKey parameter + query parameter with ingredients
       url:'http://api.spoonacular.com/recipes/complexSearch?apiKey='+ this.apiKey+'&query=' + this.ingredients  };
 
 
-      //call service get method and wait for response without satlling /freezing
-      //application should be responsive so this call is executed asynchronously - class materials
+
+      //call service get method and wait for response without stalling /freezing
+      //application should be responsive so this call is executed asynchronously - class materials wk 10
       const response = await this.recipeApi.get(options);
 
 // output response data to console for testing 
 console.log(JSON.stringify(response.data));
+
+//Parse the API response into recipes array- REF: class materials
+this.recipes = response.data.results;
+
+//Test to make sure parsing worked 
+console.log('Recipes array:', this.recipes);
+
   }
 }
